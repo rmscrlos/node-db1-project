445ec708137eb656e9c76cf2430d5a8a436a36df
Carlos Ramos <rmscrlos@gmail.com>
2021-02-24 23:28:44 -0500

    completed getAll function in accounts-model.js and completed route to get all accounts in account-router.js

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,27 +1,30 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
-}
+	// DO YOUR MAGIC
+	return db.select('*').from('accounts');
+};
 
 const getById = id => {
-  // DO YOUR MAGIC
-}
+	// DO YOUR MAGIC
+};
 
 const create = async account => {
-  // DO YOUR MAGIC
-}
+	// DO YOUR MAGIC
+};
 
 const updateById = async (id, account) => {
-  // DO YOUR MAGIC
-}
+	// DO YOUR MAGIC
+};
 
 const deleteById = async id => {
-  // DO YOUR MAGIC
-}
+	// DO YOUR MAGIC
+};
 
 module.exports = {
-  getAll,
-  getById,
-  create,
-  updateById,
-  deleteById,
-}
+	getAll,
+	getById,
+	create,
+	updateById,
+	deleteById
+};
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,31 +1,39 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const accountModel = require('./accounts-model');
 
 router.get('/', async (req, res, next) => {
-  // DO YOUR MAGIC
-})
+	// DO YOUR MAGIC
+	try {
+		const accounts = await accountModel.getAll();
+		res.json(accounts);
+	} catch (err) {
+		next(err);
+	}
+});
 
 router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+	// DO YOUR MAGIC
+});
 
 router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+	// DO YOUR MAGIC
+});
 
 router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+	// DO YOUR MAGIC
 });
 
 router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+	// DO YOUR MAGIC
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
-  res.status(500).json({
-    message: 'something went wrong inside the accounts router',
-    errMessage: err.message,
-  })
-})
+router.use((err, req, res, next) => {
+	// eslint-disable-line
+	// CALL next(err) IF THE PROMISE REJECTS INSIDE YOUR ENDPOINTS
+	res.status(500).json({
+		message: 'something went wrong inside the accounts router',
+		errMessage: err.message
+	});
+});
 
 module.exports = router;
